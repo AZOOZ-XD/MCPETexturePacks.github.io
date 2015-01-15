8afefa33ae079cc702c8f9ad5a9dd981aebdb582
Herobrinemodder <eddy1270@icloud.com>
2015-01-16 10:32:20 +1300

    idk

--- template/index/devLink.php
+++
@@ -1,4 +0,0 @@
-<ul>
-    <li>
-        <a class=\"devLink\" target=\"_blank\" href="https://twitter.com/16herobrine">@ADeveloper on Twitter</a></li>
-    <li>
--- template/index/footer.php
+++
@@ -1,3 +0,0 @@
-<p><a href="http://herobrinemodder.github.io" target="_blank">Herobrinemodder's cydia repo</a></p>
-</body>
-</html>
